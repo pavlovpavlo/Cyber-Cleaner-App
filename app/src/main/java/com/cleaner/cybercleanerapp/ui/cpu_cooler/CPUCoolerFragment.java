c57bba6853ad9322f0749eda43ed8704c66bd233
Pavlo <pavlovpavlo2013@gmail.com>
2021-08-25 00:39:24 +0300

    add dimens

--- app/src/main/java/com/cleaner/cybercleanerapp/ui/cpu_cooler/CPUCoolerFragment.java
+++ app/src/main/java/com/cleaner/cybercleanerapp/ui/cpu_cooler/CPUCoolerFragment.java
@@ -1,7 +1,12 @@
 package com.cleaner.cybercleanerapp.ui.cpu_cooler;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
+import android.animation.ValueAnimator;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,28 +16,96 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.cleaner.cybercleanerapp.R;
 
-public class CPUCoolerFragment extends Fragment {
+import cn.septenary.ui.widget.GradientProgressBar;
 
+public class CPUCoolerFragment extends Fragment {
     private CPUCoolerViewModel mViewModel;
+    private ImageView img_crcle;
+    private View view_root;
+    private GradientProgressBar bar;
+    private LinearLayout button;
+    private ConstraintLayout btn_form;
+    private ImageView image_blick;
 
     public static CPUCoolerFragment newInstance() {
         return new CPUCoolerFragment();
     }
-
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.cpu_cooler_fragment, container, false);
-    }
+        view_root=inflater.inflate(R.layout.cpu_cooler_fragment, container, false);
+        ImageView iv = view_root.findViewById(R.id.image_circle_1);
+        ObjectAnimator scaleDown = ObjectAnimator.ofPropertyValuesHolder(
+                iv,
+                PropertyValuesHolder.ofFloat("scaleX", 0.9f),
+                PropertyValuesHolder.ofFloat("scaleY", 0.9f));
+        scaleDown.setDuration(500);
+        scaleDown.setRepeatCount(ObjectAnimator.INFINITE);
+        scaleDown.setRepeatMode(ObjectAnimator.REVERSE);
+        scaleDown.start();
 
+        initView();
+
+        return view_root;
+    }
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(CPUCoolerViewModel.class);
         // TODO: Use the ViewModel
+    }
+
+    private void initView(){
+        bar=view_root.findViewById(R.id.bar);
+        button=view_root.findViewById(R.id.btn_start);
+        image_blick=view_root.findViewById(R.id.image_blick);
+        btn_form=view_root.findViewById(R.id.btn_form);
+        bar.setProgress(90,true);
+        starAnimBtn();
+
+
+    }
+
+    private void starAnimBtn(){
+
+        final ValueAnimator translateAnimator = ValueAnimator.ofFloat(0.0f, 1.0f);
+        ObjectAnimator flashAnimator = ObjectAnimator.ofFloat(image_blick, "alpha", 0.0f, 1.0f);
+
+        //  translateAnimator.setDuration(2000);  // animation duration
+        translateAnimator.setRepeatCount(5);  // animation repeat count
+        translateAnimator.setRepeatMode(ValueAnimator.REVERSE);
+        translateAnimator.setRepeatCount(Animation.INFINITE);
+
+        translateAnimator.setDuration(200);
+        //translateAnimator.setRepeatCount(5);
+        flashAnimator.setRepeatMode(ValueAnimator.REVERSE);
+        flashAnimator.setRepeatCount(Animation.INFINITE);
+
+
+        final AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.playTogether(flashAnimator,translateAnimator);
+        animatorSet.setDuration(2000);
+
+        final float x = image_blick.getX();
+        final float y = image_blick.getY();
+        translateAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                float t = (Float) translateAnimator.getAnimatedValue();
+                image_blick.setTranslationX(x + t*255);    // do your own
+                //  image_blick.setTranslationY(y + t*100);    // thing here
+
+            }
+        });
+
+        animatorSet.start();
+
     }
 
 }
--- app/src/main/java/com/cleaner/cybercleanerapp/ui/phone_booster/PhoneBoosterFragment.java
+++ app/src/main/java/com/cleaner/cybercleanerapp/ui/phone_booster/PhoneBoosterFragment.java
@@ -1,7 +1,12 @@
 package com.cleaner.cybercleanerapp.ui.phone_booster;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
+import android.animation.ValueAnimator;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,13 +16,25 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.cleaner.cybercleanerapp.R;
+import com.cleaner.cybercleanerapp.ui.cpu_cooler.CPUCoolerViewModel;
+
+import cn.septenary.ui.widget.GradientProgressBar;
 
 public class PhoneBoosterFragment extends Fragment {
 
     private PhoneBoosterViewModel mViewModel;
 
+    private ImageView img_crcle;
+    private View view_root;
+    private GradientProgressBar bar;
+    private LinearLayout button;
+    private ConstraintLayout btn_form;
+    private ImageView image_blick;
     public static PhoneBoosterFragment newInstance() {
         return new PhoneBoosterFragment();
     }
@@ -25,7 +42,19 @@ public class PhoneBoosterFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.phone_booster_fragment, container, false);
+        view_root=inflater.inflate(R.layout.phone_booster_fragment, container, false);
+        ImageView iv = view_root.findViewById(R.id.image_circle_1);
+        ObjectAnimator scaleDown = ObjectAnimator.ofPropertyValuesHolder(
+                iv,
+                PropertyValuesHolder.ofFloat("scaleX", 0.9f),
+                PropertyValuesHolder.ofFloat("scaleY", 0.9f));
+        scaleDown.setDuration(500);
+        scaleDown.setRepeatCount(ObjectAnimator.INFINITE);
+        scaleDown.setRepeatMode(ObjectAnimator.REVERSE);
+        scaleDown.start();
+
+        initView();
+        return view_root;
     }
 
     @Override
@@ -35,4 +64,50 @@ public class PhoneBoosterFragment extends Fragment {
         // TODO: Use the ViewModel
     }
 
+    private void initView(){
+        bar=view_root.findViewById(R.id.bar);
+        button=view_root.findViewById(R.id.btn_start);
+        image_blick=view_root.findViewById(R.id.image_blick);
+        btn_form=view_root.findViewById(R.id.btn_form);
+        bar.setProgress(90,true);
+        starAnimBtn();
+
+
+    }
+
+    private void starAnimBtn(){
+
+        final ValueAnimator translateAnimator = ValueAnimator.ofFloat(0.0f, 1.0f);
+        ObjectAnimator flashAnimator = ObjectAnimator.ofFloat(image_blick, "alpha", 0.0f, 1.0f);
+
+        //  translateAnimator.setDuration(2000);  // animation duration
+        translateAnimator.setRepeatCount(5);  // animation repeat count
+        translateAnimator.setRepeatMode(ValueAnimator.REVERSE);
+        translateAnimator.setRepeatCount(Animation.INFINITE);
+
+        translateAnimator.setDuration(200);
+        //translateAnimator.setRepeatCount(5);
+        flashAnimator.setRepeatMode(ValueAnimator.REVERSE);
+        flashAnimator.setRepeatCount(Animation.INFINITE);
+
+
+        final AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.playTogether(flashAnimator,translateAnimator);
+        animatorSet.setDuration(2000);
+
+        final float x = image_blick.getX();
+        final float y = image_blick.getY();
+        translateAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                float t = (Float) translateAnimator.getAnimatedValue();
+                image_blick.setTranslationX(x + t*255);    // do your own
+                //  image_blick.setTranslationY(y + t*100);    // thing here
+
+            }
+        });
+
+        animatorSet.start();
+
+    }
 }
